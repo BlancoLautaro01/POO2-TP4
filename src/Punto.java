57872e762ea055bae3c8ca537023b99cbae04e9b
Lautaro Blanco <blanco.lautaro@outlook.com>
2020-04-11 22:00:33 -0300

    Commit inicial

---
+++ src/Punto.java
@@ -0,0 +1,28 @@
+
+public class Punto {
+	
+	private float x;
+	private float y;
+	
+	public Punto(float x, float y) {
+		super();
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+}
